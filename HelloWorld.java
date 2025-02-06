7c11b29f26cc76e2bf21c2afec11c4e9b5c4e7b4
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-05 19:29:08 -0500

    Spiced it up a little bit.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
-
 public class HelloWorld {
-
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		System.out.println("I have become Death, the Destroyer of Worlds!");
 	}
-
 }
